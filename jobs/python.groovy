eb8bcac4d486d97a0f7ed8c1cbd6f276bb7a6400
Ubuntu <ubuntu@ip-172-31-13-21.us-east-2.compute.internal>
2018-08-26 21:01:58 +0000

    python groovy file

---
+++ jobs/python.groovy
@@ -0,0 +1,27 @@
+project_name= "jenkins-pipeline-python"
+repo = "git@github.com:hacosta333/jenkinstest.git"
+repo_name= "repo"
+
+
+pipelineJob(project_name){
+   definition{
+      triggers{
+        scm('* * * * *')
+      }
+
+      cpsScm{
+        scm{
+          git{
+             remote{
+                name(repo_name)
+                url(repo)
+             }
+          }
+          scriptPath("Jenkinsfile")
+        }
+      }
+   }
+}
+
+
+}
